4ab514c3c115dc1d3f20d864212c4438eb5c630e
Daniel Mutuku <danielmakau507@gmail.com>
2023-11-24 12:47:23 +0300

    Completed Assignment

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -2,6 +2,10 @@
 
 def return_evens(num_list):
     pass
+    even_list = [item for item in num_list if item % 2 == 0]
+    return even_list
 
 def make_exclamation(sentence_list):
     pass
+    new_list = [word + '!' for word in sentence_list]
+    return new_list
